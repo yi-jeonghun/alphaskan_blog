c53c7dc45cbb4b5d4463f338fadb8b499e1b3049
Jeonghun Lee <yi.jeonghun@gmail.com>
2026-08-16 10:53:31 +0900

    header, footer 작업. 불필요 삭제

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'AlphaSkan';
+export const SITE_DESCRIPTION = 'AI와 데이터로 한국 주식시장을 분석하고 실험하는 개발 기록';
